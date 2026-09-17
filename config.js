3368869fd4d50c7ece16e08a372470ca5e3b2593
inipallqqqvvip-wq <inipallqqqvvip@gmail.com>
2026-09-17 20:25:59 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  BOT_TOKEN: "8030330458:AAED1S8oO4RIVO_at1C1N8Km3tiUpAkPTMM",
+  OWNER_ID: ["7873239475"],
+  LOC: "1",
+  EGG: "15",
+  PLTA: "",
+  TOKEN: "",
+  DOMAIN: "",
+};
